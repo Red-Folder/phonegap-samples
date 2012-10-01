a14b0edd9b3722e1e36c435f4105950f815fa40b
Red-Folder <markbryantaylor@gmail.com>
2012-10-01 21:31:20 +0100

    new file:   2.0.0/TwitterExample/.classpath 	new file:   2.0.0/TwitterExample/.project 	new file:   2.0.0/TwitterExample/AndroidManifest.xml 	new file:   2.0.0/TwitterExample/assets/www/backgroundService-2.0.0.js 	new file:   2.0.0/TwitterExample/assets/www/cordova-2.0.0.js 	new file:   2.0.0/TwitterExample/assets/www/handlebars-1.0.0.beta.6.js 	new file:   2.0.0/TwitterExample/assets/www/index.html 	new file:   2.0.0/TwitterExample/assets/www/jquery-1.8.1.min.js 	new file:   2.0.0/TwitterExample/assets/www/twitterService.js 	new file:   2.0.0/TwitterExample/libs/backgroundserviceplugin-2.0.0.jar 	new file:   2.0.0/TwitterExample/libs/cordova-2.0.0.jar 	new file:   2.0.0/TwitterExample/proguard-project.txt 	new file:   2.0.0/TwitterExample/project.properties 	new file:   2.0.0/TwitterExample/res/drawable-hdpi/ic_launcher.png 	new file:   2.0.0/TwitterExample/res/drawable-hdpi/ic_stat_notification.png 	new file:   2.0.0/TwitterExample/res/drawable-ldpi/ic_launcher.png 	new file:   2.0.0/TwitterExample/res/drawable-ldpi/ic_stat_notification.png 	new file:   2.0.0/TwitterExample/res/drawable-mdpi/ic_launcher.png 	new file:   2.0.0/TwitterExample/res/drawable-mdpi/ic_stat_notification.png 	new file:   2.0.0/TwitterExample/res/drawable-xhdpi/ic_launcher.png 	new file:   2.0.0/TwitterExample/res/drawable-xhdpi/ic_stat_notification.png 	new file:   2.0.0/TwitterExample/res/layout/main.xml 	new file:   2.0.0/TwitterExample/res/values/strings.xml 	new file:   2.0.0/TwitterExample/res/xml/config.xml 	new file:   2.0.0/TwitterExample/src/com/red_folder/sample/TwitterExampleActivity.java 	new file:   2.0.0/TwitterExample/src/com/red_folder/sample/TwitterService.java

---
+++ 2.0.0/TwitterExample/assets/www/twitterService.js
@@ -0,0 +1,4 @@
+cordova.define(	'cordova/plugin/twitterService',	function(require, exports, module) {    
+												CreateBackgroundService('com.red_folder.sample.TwitterService', require, exports, module);
+											});
+
